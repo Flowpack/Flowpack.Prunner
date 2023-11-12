9f8dbb7d65d68a83f670b2319f00e8c606f77c6e
Christoph Dähne <christoph.daehne@sandstorm.de>
2023-11-12 15:16:52 +0100

    FEATURE: convenience getter functions

--- Classes/Dto/Jobs.php
+++ Classes/Dto/Jobs.php
@@ -10,6 +10,7 @@ use Neos\Flow\Annotations as Flow;
  */
 class Jobs implements \IteratorAggregate
 {
+
     /**
      * @var Job[]
      */
@@ -19,7 +20,6 @@ class Jobs implements \IteratorAggregate
     {
         $this->jobs = $jobs;
     }
-
 
     public static function fromJsonArray(array $in): self
     {
@@ -43,21 +43,89 @@ class Jobs implements \IteratorAggregate
     }
 
     /**
+     * @return Jobs all scheduled jobs not yet started
+     */
+    public function waiting(): Jobs
+    {
+        return $this->filter(function (Job $job) {
+            return !$job->getStart();
+        });
+    }
+
+    /**
      * Filter running jobs
      *
      * @return Jobs
      */
     public function running(): Jobs
     {
-        $filteredJobs = [];
-        foreach ($this->jobs as $job) {
+        return $this->filter(function (Job $job) {
             // running = started jobs which have not finished.
-            if ($job->getStart() !== null && !$job->getEnd()) {
-                $filteredJobs[] = $job;
+            return $job->getStart() !== null && !$job->getEnd();
+        });
+    }
+
+    /**
+     * @return Jobs successful, canceled and failed jobs
+     */
+    public function completed(): Jobs
+    {
+        return $this->filter(function (Job $job) {
+            return $job->isCompleted();
+        });
+    }
+
+    /**
+     * @return Jobs successfully completed jobs
+     */
+    public function successful(): Jobs
+    {
+        return $this->filter(function (Job $job) {
+            return $job->isCompleted() && !$job->isErrored() && !$job->isCanceled();
+        });
+    }
+
+    /**
+     * @return Jobs canceled jobs
+     */
+    public function canceled(): Jobs
+    {
+        return $this->filter(function (Job $job) {
+            return $job->isCanceled();
+        });
+    }
+
+    /**
+     * @return Jobs failed jobs
+     */
+    public function errored(): Jobs
+    {
+        return $this->filter(function (Job $job) {
+            return $job->isErrored();
+        });
+    }
+
+    /**
+     * @param callable $predicate function(Job $job): bool
+     * @return Jobs all jobs where $predicate($job)
+     */
+    public function filter(callable $predicate): Jobs
+    {
+        $result = [];
+        foreach ($this->jobs as $job) {
+            if ($predicate($job)) {
+                $result[] = $job;
             }
         }
+        return new self($result);
+    }
 
-        return new self($filteredJobs);
+    /**
+     * @return Job[]
+     */
+    public function getArray(): array
+    {
+        return $this->jobs;
     }
 
     /**
